45dd8f97f402122422e66542dcb62870ec133fe3
Abosede Oloude Oyamine <oloudeabosede26@gmail.com>
2026-09-03 12:35:20 +0100

    started bottom nav

--- src/components/Layout/AppLayout.tsx
+++ src/components/Layout/AppLayout.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router";
+import BottomNav from "./BottomNav";
+
 function AppLayout() {
-  return <div>AppLayout</div>;
+  return <div className="flex flex-col">
+    <main className="overflow-y-auto px-4 py-6"><Outlet/></main>
+    <BottomNav/>
+  </div>;
 }
 
 export default AppLayout;
--- src/components/Layout/BottomNav.tsx
+++ src/components/Layout/BottomNav.tsx
@@ -1,5 +1,28 @@
+import { GoHomeFill } from "react-icons/go";
+import { Link } from "react-router";
+
+const links = [
+  { icon: GoHomeFill, title: "Overview", path: "/" },
+  { icon: GoHomeFill, title: "Transactions", path: "/transactions" },
+  { icon: GoHomeFill, title: "Budgets", path: "/budgets" },
+  { icon: GoHomeFill, title: "Pots", path: "/pots" },
+  { icon: GoHomeFill, title: "Recurring Bills", path: "/bills" },
+];
+
 function BottomNav() {
-  return <div>BottomNav</div>;
+  return (
+    <nav className="bg-grey900 rounded-t-2xl pt-2 px-4 fixed bottom-0 left-0 right-0 w-full flex items-center justify-between">
+      {links.map(({ icon: Icon, title, path }) => (
+        <Link
+          to={path}
+          key={title}
+          className="flex flex-col gap-1 items-center"
+        >
+          <Icon /> <span className="hidden md:inline">{title}</span>
+        </Link>
+      ))}
+    </nav>
+  );
 }
 
 export default BottomNav;
